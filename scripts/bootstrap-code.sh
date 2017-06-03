f34c55df1d96fe78891edfce9aafd58e951634c9
Aleksi Peebles <aleksi@iki.fi>
2017-06-03 03:09:48 +0000

    Comment out D7 bootstrap

--- scripts/bootstrap-code.sh
+++ scripts/bootstrap-code.sh
@@ -3,14 +3,16 @@
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${MY_DIR}/scripts.conf
 
-# Aliases for global Drush
+# Create global Drush configuration directory
 sudo mkdir -p /etc/drush
-sudo cp ${D8_DIR}/drush/site-aliases/* /etc/drush
-sudo cp ${D7_DIR}/drush/site-aliases/* /etc/drush 2>/dev/null
 
-##
+
+################################################################################
 # Drupal 8
-##
+################################################################################
+
+# Drush aliases
+sudo cp ${D8_DIR}/drush/site-aliases/* /etc/drush
 
 # Install core and contrib
 cd ${D8_DIR}
@@ -23,10 +25,14 @@ mkdir -p ${D8_SITES_DIR}/shila.dev/themes
 cd ${D8_SITES_DIR}/shila.dev/themes
 (cd shila_theme && git pull) || git clone https://github.com/aleksip/shila-drupal-theme shila_theme
 
-##
+
+################################################################################
 # Drupal 7
-##
+################################################################################
+
+# Drush aliases
+#sudo cp ${D7_DIR}/drush/site-aliases/* /etc/drush 2>/dev/null
 
 # Install core and contrib
-${D7_SCRIPTS_DIR}/update-core.sh
-${D7_SCRIPTS_DIR}/update-contrib.sh
+#${D7_SCRIPTS_DIR}/update-core.sh
+#${D7_SCRIPTS_DIR}/update-contrib.sh
