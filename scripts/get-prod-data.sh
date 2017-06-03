13d1e779a89ae3a85a2f22817e9ab71b8addedb6
Aleksi Peebles <aleksi@iki.fi>
2017-06-03 03:16:07 +0000

    Add REMOTE_SHILA_ROOT variable

--- scripts/get-prod-data.sh
+++ scripts/get-prod-data.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 
-REMOTE_DATA_DIR=/var/www/shila-prod/data/
 MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${MY_DIR}/scripts.conf
 
 mkdir -p ${DATA_DIR}
 
 echo "running remote db-dump"
-ssh user@example.com '/var/www/shila-prod/code/shila-drupal/scripts/db-dump.sh'
+ssh user@example.com '${REMOTE_SHILA_ROOT}/code/shila-drupal/scripts/db-dump.sh'
 
 echo "running rsync"
-rsync -avz -e ssh user@example.com:${REMOTE_DATA_DIR} ${DATA_DIR}
+rsync -avz -e ssh user@example.com:${REMOTE_SHILA_ROOT}/data/ ${DATA_DIR}
